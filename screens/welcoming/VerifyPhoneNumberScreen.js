e4b51035a3b25c189c943516112e2d9bde03489e
Tamara A <akira-49-q@yandex.ru>
2022-08-12 12:46:52 +0300

    feat: add verification screen

--- screens/welcoming/VerifyPhoneNumberScreen.js
+++ screens/welcoming/VerifyPhoneNumberScreen.js
@@ -1,17 +1,129 @@
-import * as React from 'react';
-import {View, Button} from 'react-native';
-import TitleText from '../../components/text/TitleText';
+import React, {useState, useRef} from 'react';
+import {View, StyleSheet, TextInput} from 'react-native';
+import TitleBoldText from '../../components/text/TitleBoldText';
+import SmallText from '../../components/text/SmallText';
+import ScreenView from '../../components/Screen';
+import {IconMessage} from '../../components/icons/UXIcons';
+import {COLORS} from '../../styles/defaultColors';
+import MainButton from '../../components/buttons/MainButton';
+
+const phoneNumber = '(000)000-0000';
+const SPACING = 10;
 
 function VerifyPhoneNumberScreen({navigation}) {
+  const inputsRef = [useRef(), useRef(), useRef(), useRef()];
+
+  const [code, setCode] = useState([]);
+  const [disabled, setDisabled] = useState(true);
+
+  /** */
+  function handleChange(value, index) {
+    setCode(prev => {
+      const newCode = [...prev];
+      if (newCode.length === index) {
+        newCode.push(value);
+      } else {
+        newCode[index] = value;
+      }
+      return newCode;
+    });
+    inputsRef[index].blur();
+    inputsRef[index + 1].focus();
+  }
+
+  /** */
+  function handleLast(value) {
+    setCode(prev => {
+      const newCode = [...prev];
+      if (newCode.length === 3) {
+        newCode.push(value);
+      } else {
+        newCode[3] = value;
+      }
+      return newCode;
+    });
+    inputsRef[3].blur();
+    setDisabled(false);
+  }
+
+  /** */
   return (
-    <View style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
-      <TitleText>Verify Phone Number Screen</TitleText>
-      <Button
-        title="Go to Location"
-        onPress={() => navigation.navigate('Location')}
-      />
-    </View>
+    <ScreenView
+      style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
+      <View style={styles.screen}>
+        <View style={styles.info}>
+          <View style={styles.icon}>
+            <IconMessage fill={COLORS.color8} />
+          </View>
+          <View style={styles.title}>
+            <TitleBoldText textColor="light">
+              Verify your phone number
+            </TitleBoldText>
+          </View>
+          <SmallText textColor="light">
+            Enter 4-digit code sent to your mobile number
+          </SmallText>
+          <SmallText textColor="light">{phoneNumber}</SmallText>
+        </View>
+        <View style={styles.inputsConteiner}>
+          {inputsRef.map((item, index) => {
+            return (
+              <TextInput
+                key={index}
+                ref={r => (inputsRef[index] = r)}
+                style={styles.input}
+                maxLength={1}
+                keyboardType="numeric"
+                value={code && code[index]}
+                onChangeText={value => {
+                  index < 3 ? handleChange(value, index) : handleLast(value);
+                }}
+              />
+            );
+          })}
+        </View>
+        <MainButton
+          title="Continue"
+          disabled={disabled}
+          onPress={() => !disabled && navigation.navigate('App')}
+        />
+      </View>
+    </ScreenView>
   );
 }
 
 export default VerifyPhoneNumberScreen;
+
+const styles = StyleSheet.create({
+  screen: {
+    height: '80%',
+  },
+  icon: {
+    width: 60,
+    height: 60,
+    backgroundColor: COLORS.color10,
+    justifyContent: 'center',
+    alignItems: 'center',
+    borderRadius: 10,
+    marginBottom: SPACING * 2,
+  },
+  info: {
+    marginBottom: SPACING * 2,
+  },
+  title: {
+    marginBottom: SPACING,
+  },
+  inputsConteiner: {
+    flexDirection: 'row',
+    justifyContent: 'center',
+  },
+  input: {
+    margin: SPACING,
+    backgroundColor: COLORS.color2,
+    borderRadius: 10,
+    paddingHorizontal: SPACING * 2,
+    paddingVertical: SPACING,
+    fontSize: 20,
+    textAlign: 'center',
+  },
+});
